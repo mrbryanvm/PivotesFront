f54956e93c5a116ce047fa68c58d8a9b0c66be2e
Vicente Guzman Aliza Abigail <alizaabigailvicenteguzman@gmail.com>
2025-04-22 17:58:36 -0400

    Agregada funcionalidad de cierre de sesión al Header

--- my-app/app/components/Header.tsx
+++ my-app/app/components/Header.tsx
@@ -1,7 +1,14 @@
-'use Client';
+'use client';
+
 import Link from 'next/link';
+import { useState } from 'react';
+import { FiLogOut } from 'react-icons/fi';
+import { useAuth } from '../lib/authContext';
 
 export default function Header() {
+  const { logout } = useAuth();
+  const [showLogout, setShowLogout] = useState(false);
+
   return (
     <header className="flex items-center justify-between p-4 bg-white shadow-md">
       {/* Logo */}
@@ -9,15 +16,28 @@ export default function Header() {
         <div className="text-2xl font-bold text-orange-500">REDIBO</div>
       </Link>
 
-      {/* Ícono de usuario */}
-      <div className="flex items-center">
-        <Link href="/profile">
-          <div className="w-10 h-10 rounded-full border-2 border-orange-500 flex items-center justify-center">
-            <span className="text-orange-500">👤</span>
-          </div>
-        </Link>
+      {/* contenedor icono*/}
+      <div className="fixed top-4 right-4 flex items-center space-x-2">
 
-        
+        {/* Botón "Cerrar sesión"  */}
+        {showLogout && (
+          <button
+            onClick={logout}
+            className="flex items-center gap-2 px-4 py-2 bg-white border border-gray-200 rounded-xl shadow hover:bg-orange-100 text-black font-medium transition-all duration-300"
+          >
+            <FiLogOut className="text-xl" />
+            <span>Cerrar sesión</span>
+          </button>
+        )}
+
+        {/* Ícono de usuario*/}
+        <button
+          onClick={() => setShowLogout(!showLogout)}
+          className="w-10 h-10 rounded-full border-2 border-orange-500 flex items-center justify-center hover:bg-orange-100"
+          title="Opciones de usuario"
+        >
+          <span className="text-orange-500">👤</span>
+        </button>
       </div>
     </header>
   );
