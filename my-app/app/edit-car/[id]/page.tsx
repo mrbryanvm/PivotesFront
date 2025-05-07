0014a6c39163e01fd430f8e8b103280745cc398e
lets04 <letiloredosalazar2004@hotmail.com>
2025-05-07 10:25:39 -0400

    correccion del formulario de editar auto para que envie los datos

--- my-app/app/edit-car/[id]/page.tsx
+++ my-app/app/edit-car/[id]/page.tsx
@@ -14,6 +14,8 @@ interface Car {
   pricePerDay: number;
   seats: number;
   transmission: string;
+  kilometers: string;
+  fuelType: string;
   color: string;
   imageUrl: string;
   isAvailable: boolean;
@@ -34,6 +36,9 @@ export default function EditCar() {
     pricePerDay: "",
     seats: "",
     color: "",
+    kilometers: "",
+    fuelType: "",
+    transmission: "",
   });
   const [error, setError] = useState<string | null>(null);
   useEffect(() => {
@@ -50,52 +55,7 @@ export default function EditCar() {
 
     loadCar();
   }, [id, token]);
-
-  // Funciones de validación
-  const validateTextField = (name: string, value: string) => {
-    if (!value.trim()) {
-      if (name === "brand") {
-        return `La marca es obligatoria`;
-      } else {
-        return `El modelo es obligatorio`;
-      }
-    }
-
-    // Validar que no contenga números ni caracteres especiales solo para la "marca"
-    if (name === "brand") {
-      if (/\d/.test(value)) {
-        return `La marca no puede contener números`;
-      }
-      if (/[^a-zA-Z\s]/.test(value)) {
-        return `La marca no puede contener caracteres especiales`;
-      }
-    }
-
-    // Validar que no contenga caracteres especiales para el "modelo"
-    if (name === "model") {
-      if (/[^a-zA-Z0-9\s]/.test(value)) {
-        return `El modelo no puede contener caracteres especiales`;
-      }
-    }
-
-    return "";
-  };
-
-  const validateYear = (value: number) => {
-    const currentYear = new Date().getFullYear();
-
-    // Verificar si el valor tiene exactamente 4 dígitos
-    if (value.toString().length !== 4) {
-      return "El año debe tener exactamente 4 dígitos";
-    }
-
-    if (value < 1900) return "El año no puede ser menor a 1900";
-    if (value > currentYear)
-      return `El año no puede ser mayor a ${currentYear}`;
-
-    return "";
-  };
-
+  
   const validatePricePerDay = (value: number) => {
     if (value <= 0) return "El precio debe ser mayor a 0";
     return "";
@@ -106,6 +66,12 @@ export default function EditCar() {
       return "La capacidad debe ser entre 1 y 20 asientos";
     return "";
   };
+
+  const validateKilometers = (value: number) => {
+    if (isNaN(value) || value < 0) return "El kilometraje debe ser un número válido";
+    return "";
+  };
+  
 
   const validateColor = (value: string) => {
     if (!value.trim()) return "El color es obligatorio";
@@ -134,33 +100,8 @@ export default function EditCar() {
     }
   };
 
-  const [yearError, setYearError] = useState("");
-  const validacionAño = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
-    if (value.length > 4) return;
-
-    const year = parseInt(value);
-    const currentYear = new Date().getFullYear();
-
-    if (!isNaN(year)) {
-      if (year < 1900) {
-        setYearError("El año no puede ser menor a 1900");
-      } else if (year > currentYear) {
-        setYearError(`El año no puede ser mayor a ${currentYear}`);
-      } else {
-        setYearError("");
-      }
-    } else {
-      setYearError("Ingresa un año válido");
-    }
-
-    setFormData({
-      ...formData,
-      [e.target.name]: value,
-    });
-  };
   const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = e.target;
 
@@ -173,16 +114,18 @@ export default function EditCar() {
 
     // Validar cada campo según su tipo
     let errorMessage = "";
-    if (name === "brand" || name === "model") {
-      errorMessage = validateTextField(name, value);
-    } else if (name === "year") {
-      errorMessage = validateYear(Number(value));
-    } else if (name === "pricePerDay") {
+    if (name === "pricePerDay") {
       errorMessage = validatePricePerDay(Number(value));
     } else if (name === "seats") {
       errorMessage = validateSeats(Number(value));
     } else if (name === "color") {
       errorMessage = validateColor(value);
+    } else if (name === "kilometers") {
+      errorMessage = validateKilometers(Number(value));
+    }else if (name === "transmission") {
+      errorMessage = value ? "" : "La transmisión es obligatoria";
+    } else if (name === "fuelType") {
+      errorMessage = value ? "" : "El combustible es obligatorio";
     }
 
     // Actualizar el estado de los errores
@@ -200,6 +143,7 @@ export default function EditCar() {
     if (!token) return;
 
     try {
+      console.log("Datos a enviar:", formData);
       await updateCar(Number(id), formData, token);
       toast.success("¡Se guardo correctamente!");
       router.push("/my-cars");
@@ -222,89 +166,30 @@ export default function EditCar() {
           className="grid grid-cols-1 md:grid-cols-2 gap-6"
         >
           <div className="md:col-span-1">
-            <h2 className="text-xl font-bold mb-4 uppercase">Información</h2>
+            <h2 className="text-xl font-bold mb-4 uppercase">INFORMACIÓN</h2>
             <div className="grid grid-cols-2 gap-4 mb-4">
               <div>
-                {/* Marca */}
-                <label className="block text-gray-600 mb-1">Marca</label>
+                {/* Color */}
+                <label className="block text-gray-600 mb-1">Color *</label>
                 <input
                   type="text"
-                  name="brand"
-                  value={formData.brand || ""}
+                  name="color"
+                  value={formData.color || ""}
                   onChange={handleChange}
                   onKeyDown={handleKeyDown}
                   className="mt-1 block w-full p-3 border rounded-2xl shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-400"
                 />
-                {formErrors.brand && (
+                {formErrors.color && (
                   <p className="text-red-500 text-sm mt-1">
-                    {formErrors.brand}
+                    {formErrors.color}
                   </p>
-                )}
-              </div>
-
-              {/* Modelo */}
-              <div>
-                <label className="block text-gray-600 mb-1">Modelo</label>
-                <input
-                  type="text"
-                  name="model"
-                  value={formData.model || ""}
-                  onChange={handleChange}
-                  onKeyDown={handleKeyDown}
-                  className="mt-1 block w-full p-3 border rounded-2xl shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-400"
-                />
-                {formErrors.model && (
-                  <p className="text-red-500 text-sm mt-1">
-                    {formErrors.model}
-                  </p>
-                )}
-              </div>
-            </div>
-
-            {/* Tipo e auto */}
-            <div>
-              <label className="block text-sm font-medium">Tipo de Auto</label>
-              <select
-                name="category"
-                value={formData.category || ""}
-                onChange={handleChange}
-                className="mt-1 block w-full p-3 border rounded-2xl shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-400"
-              >
-                <option value="">Selecciona un tipo de auto </option>
-                <option value="Sedan">Sedán</option>
-                <option value="Camioneta">Camioneta</option>
-                <option value="SUV">SUV</option>
-                <option value="Deportivo">Deportivo</option>
-                <option value="Eléctrico">Eléctrico</option>
-              </select>
-            </div>
-
-            <div className="grid grid-cols-2 gap-4 mb-4">
-              {/* Año */}
-              <div>
-                <label className="block text-gray-600 mb-1">Año</label>
-                <input
-                  type="number"
-                  name="year"
-                  value={formData.year || ""}
-                  onChange={validacionAño}
-                  onKeyDown={(e) =>
-                    ["e", "E", "+", "-", "."].includes(e.key) &&
-                    e.preventDefault()
-                  }
-                  className={`mt-1 block w-full p-3 border rounded-2xl shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-400 ${
-                    yearError ? "border-red-500" : "border-gray-300"
-                  }`}
-                />
-                {yearError && (
-                  <p className="text-red-500 text-sm mt-1">{yearError}</p>
                 )}
               </div>
 
               {/* Precio por día */}
               <div>
-                <label className="block text-sm font-medium">
-                  Precio por Día
+                <label className="block text-gray-600 mb-1">
+                  Precio por día *
                 </label>
                 <input
                   type="number"
@@ -324,19 +209,67 @@ export default function EditCar() {
               </div>
             </div>
 
-            {/* Color */}
+            {/* Kilometraje */}
             <div>
-              <label className="block text-sm font-medium">Color</label>
+              <label className="block text-sm font-medium">Kilometraje *</label>
               <input
                 type="text"
-                name="color"
-                value={formData.color || ""}
+                name="kilometers"
+                value={formData.kilometers || ""}
+                onChange={handleChange}
+                className="mt-1 block w-full p-3 border rounded-2xl shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-400"
+              />
+              {formErrors.kilometers && (
+                <p className="text-red-500 text-sm mt-1">
+                  {formErrors.kilometers}
+                </p>
+              )}
+            </div>
+
+            <div className="grid grid-cols-2 gap-4 mb-4">
+            </div>
+
+            <h2 className="text-xl font-bold mb-4 uppercase">EQUIPAMIENTO</h2>
+            {/* Transmision */}
+            <div>
+              <label className="block text-sm font-medium">Transmisión *</label>
+              <select
+                name="transmission"
+                value={formData.transmission || ""}
                 onChange={handleChange}
                 onKeyDown={handleKeyDown}
                 className="mt-1 block w-full p-3 border rounded-2xl shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-400"
-              />
-              {formErrors.color && (
-                <p className="text-red-500 text-sm mt-1">{formErrors.color}</p>
+              >
+                <option value="">Seleccionar</option>
+                <option value="Manual">Manual</option>
+                <option value="Automático">Automático</option>
+              </select>
+              {formErrors.transmission && (
+                <p className="text-red-500 text-sm mt-1">
+                  {formErrors.transmission}
+                </p>
+              )}
+            </div>
+
+            {/* Combustible*/}
+            <div>
+              <label className="block text-sm font-medium">Combustible *</label>
+              <select
+                name="fuelType"
+                value={formData.fuelType || ""}
+                onChange={handleChange}
+                onKeyDown={handleKeyDown}
+                className="mt-1 block w-full p-3 border rounded-2xl shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-400 "
+              >
+                <option value="">Seleccionar</option>
+              <option value="Gas">Gas</option>
+              <option value="Gasolina">Gasolina</option>
+              <option value="Eléctrico">Eléctrico</option>
+              </select>
+              {formErrors.fuelType && (
+                <p className="text-red-500 text-sm mt-1">
+                  {formErrors.fuelType}
+                </p>
               )}
             </div>
           </div>
@@ -356,7 +289,10 @@ export default function EditCar() {
             </div>
             {/* Imagen */}
             <div className="md:col-span-2">
-              <label className="text-xl font-bold mb-7 uppercase"> FOTOS(URL)</label>
+              <label className="text-xl font-bold mb-7 uppercase">
+                {" "}
+                FOTOS(URL)
+              </label>
               <input
                 type="text"
                 name="imageUrl"
@@ -368,22 +304,20 @@ export default function EditCar() {
           </div>
           {/* Botones */}
           <div className="flex justify-end gap-4 mt-6">
-        <button
-          type="button"
-          className="border border-orange-500 text-orange-500 px-8 py-2 rounded"
-          onClick={() => router.push("/my-cars")}
-        >
-          Cancelar
-        </button>
-        <button
-          form="formulario"
-          type="submit"
-          className="bg-orange-500 text-white px-8 py-2 rounded"
-
-        >
-          Guardar
-        </button>
-      </div>
+            <button
+              type="button"
+              className="border border-orange-500 text-orange-500 px-8 py-2 rounded"
+              onClick={() => router.push("/my-cars")}
+            >
+              Cancelar
+            </button>
+            <button
+              type="submit"
+              className="bg-orange-500 text-white px-8 py-2 rounded"
+            >
+              Guardar
+            </button>
+          </div>
         </form>
       </div>
     </div>
